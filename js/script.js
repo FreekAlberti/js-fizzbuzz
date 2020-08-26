f6aa3787e21b8bd846b57525a0322bf36c2ec266
FreekAlberti <federicoalberi96@gmail.com>
2020-08-26 15:39:40 +0200

    progetto completo

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
+
+var i = 1;
+
+while (i <= 100) {
+  var contenuto = document.getElementById("lista").innerHTML;
+  var sostituto = i;
+  if ((i % 3 == 0) && (i % 5 == 0)) {
+    sostituto = "FizzBuzz";
+  } else if (i % 5 == 0) {
+    sostituto = "Buzz";
+  } else if (i % 3 == 0) {
+    sostituto = "Fizz"
+  }
+  document.getElementById("lista").innerHTML = contenuto + "<li>" + sostituto + "</li>";
+  i++;
+}
